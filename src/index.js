dcab29c3db65700b04dcf1a14094d620bda589c2
mvazquezCohort14 <149540924+mvazquezCohort14@users.noreply.github.com>
2024-05-31 17:59:55 -0500

    .....

--- src/index.js
+++ src/index.js
@@ -16,23 +16,27 @@ document
   .addEventListener("click", () => {
     console.log("Hello");
 
-    document.querySelector(
-      ".temperature > p"
-    ).textContent = `Hello ${temperatureInputElement.value}`;
-
     ///change img
     const temperatureArr = ["winterWide", "fallWide", "spring@ide", "hotWide"];
 
     let temperature = temperatureArr[0];
-    console.log(temperature);
+
+    let temperatureP = `It's very cold outside.`;
 
     if (temperatureInputElement.value > 78) {
+      temperatureP = `It's HOT outside`;
       temperature = temperatureArr[3];
     } else if (temperatureInputElement.value > 68) {
+      temperatureP = `It's nice out.`;
+
       temperature = temperatureArr[2];
     } else if (temperatureInputElement.value > 55) {
+      temperatureP = `The Fall.... My Fav.`;
+
       temperature = temperatureArr[1];
     }
+
+    document.querySelector(".temperature > p").textContent = temperatureP;
 
     document.querySelector(
       ".aside > picture > img"
