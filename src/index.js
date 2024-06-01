cddf15d07dc1d867a5f559dc949783535e64ad96
mvazquezCohort14 <149540924+mvazquezCohort14@users.noreply.github.com>
2024-06-01 02:41:24 -0500

    .....

--- src/index.js
+++ src/index.js
@@ -1,44 +1,17 @@
-///submit
-document.querySelector("form").addEventListener("submit", (e) => {
-  e.preventDefault();
-  document.querySelector("section > p ").textContent = `${e.target[0].value}`;
+///submit form. username password
+
+let userEmail = "lly@icstars.org";
+let password = "password@1";
+
+const formElement = document.querySelector("form");
+formElement.addEventListener("submit", (e) => {
+  // e.preventDefault();
+
+  if (e.target[0].value == "lly@icstars.org" && e.target[1].value == password) {
+    formElement.action = "./src/home.html";
+    // formElement.reset();
+    // formElement.target = "_blank";
+  } else {
+    alert("credentials are incorrect");
+  }
 });
-
-///temperature
-
-///grab, temperature input element
-const temperatureInputElement = document.querySelector(".temperature > input");
-console.log(temperatureInputElement);
-
-///grab temperature submit button
-document
-  .querySelector(".temperature > button")
-  .addEventListener("click", () => {
-    console.log("Hello");
-
-    ///change img
-    const temperatureArr = ["winterWide", "fallWide", "spring@ide", "hotWide"];
-
-    let temperature = temperatureArr[0];
-
-    let temperatureP = `It's very cold outside.`;
-
-    if (temperatureInputElement.value > 78) {
-      temperatureP = `It's HOT outside`;
-      temperature = temperatureArr[3];
-    } else if (temperatureInputElement.value > 68) {
-      temperatureP = `It's nice out.`;
-
-      temperature = temperatureArr[2];
-    } else if (temperatureInputElement.value > 55) {
-      temperatureP = `The Fall.... My Fav.`;
-
-      temperature = temperatureArr[1];
-    }
-
-    document.querySelector(".temperature > p").textContent = temperatureP;
-
-    document.querySelector(
-      ".aside > picture > img"
-    ).src = `./assets/${temperature}.jpg`;
-  });
---
+++ src/weather.js
@@ -0,0 +1,46 @@
+// /temperature
+
+///grab the select element, option element
+const selectElement = document.querySelector("select");
+///loop to add the options the select element
+for (let i = 0; i < 120; i++) {
+  const optionElement = document.createElement("option");
+  optionElement.value = i;
+  optionElement.textContent = i;
+  selectElement.appendChild(optionElement);
+}
+
+// /selectElement.addEvent?
+selectElement.addEventListener("click", () => {
+  ///change img
+  const temperatureArr = ["winterWide", "fallWide", "spring@ide", "hotWide"];
+
+  let temperature = temperatureArr[0];
+
+  let temperatureP = `It's very cold outside.`;
+
+  if (selectElement.value > 78) {
+    temperatureP = `It's HOT outside`;
+    temperature = temperatureArr[3];
+  } else if (selectElement.value > 68) {
+    temperatureP = `It's nice out.`;
+
+    temperature = temperatureArr[2];
+  } else if (selectElement.value > 55) {
+    temperatureP = `The Fall.... My Fav.`;
+
+    temperature = temperatureArr[1];
+  }
+
+  document.querySelector(".temperature > p").textContent = temperatureP;
+
+  document.querySelector(
+    ".aside > picture > img"
+  ).src = `../assets/${temperature}.jpg`;
+  console.log("test");
+});
+
+///grab temperature submit button
+document
+  .querySelector(".temperature > button")
+  .addEventListener("click", () => {});
