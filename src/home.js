bf8e1cbccf196ed412a02043cd368d37f95f0f02
looSeeCan2 <nruabnrabkojthiabkuv@gmail.com>
2024-06-04 17:43:35 -0500

    .....

--- src/home.js
+++ src/home.js
@@ -7,4 +7,23 @@ console.log(isLoggedIn);
 
 // if (!isLoggedIn) {
 //   window.location.href = "/";
-// }
+//
+
+let translate = 0;
+
+const sliderElement = document.querySelector(".slider");
+
+document.querySelector(".left").addEventListener("click", () => {
+  if (translate < 0) {
+    translate += 225;
+    sliderElement.style.transform = "translateX(" + translate + "px)";
+  }
+});
+
+document.querySelector(".right").addEventListener("click", () => {
+  console.log("hello");
+  if (translate > -1500) {
+    translate -= 225;
+    sliderElement.style.transform = "translateX(" + translate + "px)";
+  }
+});
